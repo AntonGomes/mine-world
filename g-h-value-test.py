ee5319e2f6dc7c74c319c906290ec3ff41dd9955
Anthony Loughlin-Gomes <anton@c118176.wlan.net.ed.ac.uk>
2023-03-22 14:31:17 +0000

    progress on python test script

--- g-h-value-test.py
+++ g-h-value-test.py
@@ -1,17 +1,54 @@
 import subprocess
+import time
+import matplotlib.pyplot as plt
 
-g_values = [str(g) for g in range(1, 11)]
-h_values = [str(h) for h in range(1, 11)]
-g_h_values = [(1,10), (1,9), (1,8), (1,7)]
-g_h_values = zip(g_values, h_values)
+g_values = range(1, 11)
+h_values = range(1, 11)
+g_h_values = []
+
+for g in g_values:
+    for h in h_values:
+        if g>=((3*h)/4):
+            continue
+        g_h_values.append((str(g), str(h))) 
+            
+start_time = time.time()
+i=1
 
 for g, h in g_h_values:
+    print("Test", i, "of", len(g_h_values))
+    print("Elapsed time:", time.time() - start_time)
+    print("g:", g, "h:", h)
+    i+=1 
     outputs = subprocess.run(["./ff", "-E", "-g", g, "-h", h, "-o", "domain-1.pddl", "-f", "problem-hard.pddl"], stdout=subprocess.PIPE, text=True).stdout.split("\n")
     reference_line = [line for line in outputs if line.startswith("time spent")]
     if reference_line:
         ref = outputs.index(reference_line[0])
         total_time= outputs[ref+6][14:19]
         steps_taken= outputs[ref-3][6:9]
+        print(f"{steps_taken} step path found in {total_time} seconds")
         test_values = [g,h,steps_taken,total_time]
     else:
         test_values = [g,h,"-","-"]
+
+gs = [x[0] for x in test_values]
+hs = [x[1] for x in test_values]
+steps = [x[2] for x in test_values]
+times = [x[3] for x in test_values]
+
+# plot the graph for steps_taken vs. g and h
+fig, ax = plt.subplots()
+ax.scatter(gs, hs, c=steps)
+ax.set_xlabel('g')
+ax.set_ylabel('h')
+ax.set_title('Steps taken')
+plt.show()
+
+# plot the graph for total_time vs. g and h
+fig, ax = plt.subplots()
+ax.scatter(gs, hs, c=times)
+ax.set_xlabel('g')
+ax.set_ylabel('h')
+ax.set_title('Total time')
+plt.show()
+    
