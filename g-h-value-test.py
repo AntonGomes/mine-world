d5aca063a96c13dd212a751e8d6efe651cde89d9
AntonGomes <aomlgomes@gmail.com>
2023-03-24 11:34:37 +0000

    report complete

--- g-h-value-test.py
+++ g-h-value-test.py
@@ -3,8 +3,8 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-g_values = range(1, 51)
-h_values = range(1, 51)
+g_values = range(1, 21)
+h_values = range(1, 21)
 g_h_values = []
 
 for g in g_values:
@@ -15,7 +15,7 @@ for g in g_values:
             
 start_time = time.time()
 i=1
-test_values = []
+test_values = {} 
 for g, h in g_h_values:
     print("Test", i, "of", len(g_h_values))
     print("Elapsed time:", time.time() - start_time)
@@ -28,26 +28,26 @@ for g, h in g_h_values:
         total_time= outputs[ref+6][14:19]
         steps_taken= outputs[ref-3][6:9]
         print(f"{steps_taken} step path found in {total_time} seconds \n")
-        test_values.append([g,h,steps_taken,total_time])
+        test_values.update({float(g)/float(h) : [steps_taken,total_time]})
     else:
         test_values = [g,h,0,0]
 
-ghratio = [((float(x[0]))/(float(x[1]))) for x in test_values]
-steps = [float(x[2]) for x in test_values]
-times = [float(x[3]) for x in test_values]
+test_values = dict(sorted(test_values.items()))
+ghratio = list(test_values.keys())
+steps = [float(test_values[x][0]) for x in test_values]
+times = [float(test_values[x][1]) for x in test_values]
 print(ghratio)
+
 # plot the graph for steps_taken vs. g and h
 fig, ax = plt.subplots()
-ax.scatter(ghratio, steps)
+ax.plot(ghratio, steps)
 ax.set_xlabel('g/h')
 ax.set_ylabel('Steps taken')
 plt.savefig('steps_taken.png')
 
 # plot the graph for total_time vs. g and h
 fig, ax = plt.subplots()
-ax.scatter(ghratio,times)
+ax.plot(ghratio,times)
 ax.set_xlabel('g/h')
 ax.set_ylabel('Times')
 plt.savefig('times.png')
-
-   
